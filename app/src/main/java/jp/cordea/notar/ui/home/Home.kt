02c200885e841cb368bc14a5bdea344b4d8bdeaa
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-02 15:18:58 +0900

    add search bar

--- app/src/main/java/jp/cordea/notar/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/notar/ui/home/Home.kt
@@ -1,14 +1,51 @@
 package jp.cordea.notar.ui.home
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Home(viewModel: HomeViewModel) {
+    val query by viewModel.query.observeAsState("")
     Scaffold(topBar = {
-        TopAppBar(title = { Text(text = "Notar") })
+        TopAppBar(
+            contentPadding = PaddingValues(horizontal = 4.dp),
+            content = {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                    leadingIcon = {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    },
+                    placeholder = {
+                        Text(text = "Search")
+                    },
+                    singleLine = true,
+                    value = query,
+                    onValueChange = viewModel::onSearchQueryChanged,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        disabledBorderColor = Color.Transparent,
+                        errorBorderColor = Color.Transparent,
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        cursorColor = MaterialTheme.colors.onPrimary
+                    )
+                )
+            },
+        )
     }) {
     }
 }
--- app/src/main/java/jp/cordea/notar/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/cordea/notar/ui/home/HomeViewModel.kt
@@ -1,9 +1,18 @@
 package jp.cordea.notar.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+) : ViewModel() {
+    private var _query = MutableLiveData("")
+    val query: LiveData<String> get() = _query
+
+    fun onSearchQueryChanged(query: String) {
+        _query.value = query
+    }
 }
