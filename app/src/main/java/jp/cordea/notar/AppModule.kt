1a25b2c6847f3747933352684da82983e94612ab
Yoshihiro Tanaka <contact@cordea.jp>
2022-01-02 13:19:12 +0900

    provide retrofit

--- app/src/main/java/jp/cordea/notar/AppModule.kt
+++ app/src/main/java/jp/cordea/notar/AppModule.kt
@@ -1,10 +1,43 @@
 package jp.cordea.notar
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    // TODO
+    fun provideClient(token: String): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor {
+                it.proceed(
+                    it
+                        .request()
+                        .newBuilder()
+                        .addHeader("Authorization", "Bearer $token")
+                        .build()
+                )
+            }
+            .build()
+
+    @Provides
+    @ExperimentalSerializationApi
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.notion.com")
+            .addConverterFactory(
+                Json.asConverterFactory(MediaType.parse("application/json")!!)
+            )
+            .client(client)
+            .build()
 }
